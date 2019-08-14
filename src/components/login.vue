9718a752fb30e573e4852b0ec247ea3774b4cfa5
oukaihao <1653706552@qq.com>
2019-08-14 22:27:31 +0800

    login-finish
    登录模块完成

---
+++ src/components/login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="my-login">
+      
+    <div class="my-form">
+        <h1>用户登录</h1>
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+        :label-position="'top'"
+        
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input type="password" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')" class="submitbtn">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+          ruleForm:{
+              username:'',
+              password:''
+          },
+          rules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 6 个字符', trigger: 'change' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'change' }
+          ],
+      }
+      }
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      
+    }
+};
+</script>
+
+<style lang="less" scope>
+html,
+body {
+  height: 100%;
+}
+.my-login {
+  height: 100%;
+  background-color: rgb(50, 65, 82);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .my-form {
+    width: 450px;
+    height: 400px;
+    background-color: aliceblue;
+    padding: 40px;
+    h1{
+        margin-bottom: 30px;
+    }
+    .submitbtn{
+        width: 100%;
+        margin-top: 30px;
+    }
+    
+  }
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,33 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//导入全局样式包
+import './assets/css/basic.css'
+
+//导入路由包
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//导入elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+//导入组件
+import login from './components/login.vue'
+
+//创建路由规则
+const routes = [
+  { path:'/login',component:login},
+  { path:'',redirect:'/login'}
+]
+
+//创建路由对象
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  //挂载
+  router,
   render: h => h(App),
 }).$mount('#app')
